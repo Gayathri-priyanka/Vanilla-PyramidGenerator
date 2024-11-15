9aefa1a9a6059d48e636f8dbd1f5785becbce12c
Gayathri-priyanka <gayathripriyankanamburu1@gmail.com>
2024-11-14 19:54:07 -0500

    styling changes

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let character = "!";
 let inverted = false;
 
-function padRow(rowNumber, rowCount) {
+function padRow(rowNumber, rowCount, character) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
 function buildPyramid() {
   const rowCount = parseInt(document.getElementById("rowCount").value);
+  const character = document.getElementById("charInput").value || "*";
   
   if (isNaN(rowCount) || rowCount < 1) {
     alert("Please enter a valid number of rows.");
@@ -16,9 +16,9 @@ function buildPyramid() {
   const rows = [];
   for (let i = 1; i <= rowCount; i++) {
     if (inverted) {
-      rows.unshift(padRow(i, rowCount));
+      rows.unshift(padRow(i, rowCount, character));
     } else {
-      rows.push(padRow(i, rowCount));
+      rows.push(padRow(i, rowCount, character));
     }
   }
 
